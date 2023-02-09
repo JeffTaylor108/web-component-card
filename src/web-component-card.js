79a774647afda62910268378b14f11df367091c4
JeffTaylor108 <jeffta1080@gmail.com>
2023-02-09 13:11:11 -0500

    commit

--- src/web-component-card.js
+++ src/web-component-card.js
@@ -8,46 +8,104 @@ class WebComponentCard extends LitElement {
   };
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--web-component-card-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
+  .card {
+    border-style: solid;
+    border-color: #355C7D;
+    background-color: #A5D8DD;
+    padding: 8px;
+    border-width: 2px;
+    border-radius: 15px;
+    width: 598px;
+    margin: 8px;
+  }
+  .heading {
+    margin: 8px 8px 8px 248px;
+  }
+  .image {
+    height: 200px;
+    width: 200px;
+  }
+  .detailsButton {
+    text-transform: uppercase;
+    font-size: 20px;
+    color: #330099;
+    background-color: #D0D0D0;
+    margin-top: 8px;
+    padding: 8px 256px;
+  }
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .paragraph {
+    padding: 8px;
+  }
+  .paragraphHide {
+    display: none;
+  }
+  
+  .cardContainer {
+    display: flex;
+    align-items: center;
+  }
+  
+  .noBackground {
+    background-color: white;
+  }
+  
+  .detailsButton:hover {
+    background-color: #0E6655;
+  }
+  .detailsButton:focus {
+    background-color: #0E6655;
+  }
+  .cloneButton {
+    text-transform: uppercase;
+    font-size: 20px;
+    color: #330099;
+    margin-top: 10px;
+    padding: 10px 40px;
+  }
+  .cloneButton:hover {
+    background-color: #0E6655;
+  }
+  .cloneButton:focus {
+    background-color: #0E6655;
+  }
+  .backgroundButton {
+    text-transform: uppercase;
+    font-size: 20px;
+    color: #330099;
+    margin-top: 10px;
+    padding: 10px 40px;
+  }
+  .headingButton {
+    text-transform: uppercase;
+    font-size: 20px;
+    color: #330099;
+    margin-top: 10px;
+    padding: 10px 40px;
+  }
+  .deleteLastButton {
+    text-transform: uppercase;
+    font-size: 20px;
+    color: #330099;
+    margin-top: 10px;
+    padding: 10px 40px;
+  }
+  
+  @media only screen and (max-width: 800px) and (min-width: 500px) { 
+    .button1 { 
+      display: none; 
+    } 
+  }
+  @media only screen and (max-width: 500px) { 
+    .card { 
+      transform: scale(.7);
+    } 
+  }
+  
   `;
 
   constructor() {
@@ -57,30 +115,38 @@ class WebComponentCard extends LitElement {
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/WebComponentCard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+    <div class="cardContainer">
+    <div class="card">
+      <h1 class="heading"> Heading </h1>
+      <div class="container">
+          <img class ="image" src="https://pbs.twimg.com/profile_images/1496512709348798464/-19Uii-a_400x400.jpg">
+        <p class="paragraph">
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+      <div>
+        <button id="newDetailsButton" class="detailsButton"> Details </button>
+      </div>
+    </div>
+    
+    <div class="card">
+      <h1 class="heading"> Heading </h1>
+      <div class="container">
+          <img class ="image" src="https://pbs.twimg.com/profile_images/1496512709348798464/-19Uii-a_400x400.jpg">
+        <p class="paragraph">
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+      <div>
+        <button class="detailsButton"> Details </button>
+      </div>
+    </div>
+    </div>
+    <div class="buttons">
+      <button class="cloneButton">Clone Page </button>
+    <button class="backgroundButton"> Toggle Card Background Color</button>
+    <button class="headingButton"> Change Heading Text</button>
+      <button class="deleteLastButton"> Delete Last Clone</button>
+    </div>
+      
     `;
   }
 }
